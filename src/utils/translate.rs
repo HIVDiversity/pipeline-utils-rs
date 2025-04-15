414bf0f8d6de42ccab01b683eb53bc400550b52c
Daniel Le Jeune <daniel.h.lejeune@gmail.com>
2025-04-15 15:10:09 +0200

    reformatted translate file

--- src/utils/translate.rs
+++ src/utils/translate.rs
@@ -1,14 +1,14 @@
+use anyhow::{Context, Result};
+use phf::phf_map;
 use std::convert::TryInto;
 use std::io::repeat;
-use anyhow::{Result, Context};
-use phf::phf_map;
 
 const GAP_CHAR: u8 = b"-"[0];
 const FRAMESHIFT_CHAR: u8 = b"X"[0];
 const UNKNOWN_AA_CHAR: u8 = b"?"[0];
 const INCOMPLETE_AA_CHAR: u8 = b"~"[0];
 
-static CODON_TABLE: phf::Map<&[u8; 3], &[u8; 1]> = phf_map!{
+static CODON_TABLE: phf::Map<&[u8; 3], &[u8; 1]> = phf_map! {
         b"TTT" => b"F",
         b"TTC" => b"F",
         b"TTA" => b"L",
@@ -76,101 +76,112 @@ static CODON_TABLE: phf::Map<&[u8; 3], &[u8; 1]> = phf_map!{
         b"---" => b"-",
 };
 
-pub fn translate(dna_seq: &[u8], strip_gaps: bool, ignore_gap_codons: bool, drop_incomplete_codons: bool) -> Result<Vec<u8>>{
+pub fn translate(
+    dna_seq: &[u8],
+    strip_gaps: bool,
+    ignore_gap_codons: bool,
+    drop_incomplete_codons: bool,
+) -> Result<Vec<u8>> {
     let mut new_seq = dna_seq.to_vec();
-    if strip_gaps{
-        new_seq = new_seq.iter().copied().filter(|character| *character != GAP_CHAR).collect();
+    if strip_gaps {
+        new_seq = new_seq
+            .iter()
+            .copied()
+            .filter(|character| *character != GAP_CHAR)
+            .collect();
     }
 
-    let mut amino_acids = Vec::with_capacity(new_seq.len()/3);
+    let mut amino_acids = Vec::with_capacity(new_seq.len() / 3);
     for codon in new_seq.chunks(3) {
-
         // If the codon is not a multiple of 3, we will always want to replace it with an incomplete amino acid, so we don't need to
         // check anything else.
 
         if codon.len() != 3 {
             if !drop_incomplete_codons {
-                log::warn!("The codon {:?} had a length of {} so we're adding a {:?}", String::from_utf8(codon.to_vec())?, codon.len(), INCOMPLETE_AA_CHAR as char);
+                log::warn!(
+                    "The codon {:?} had a length of {} so we're adding a {:?}",
+                    String::from_utf8(codon.to_vec())?,
+                    codon.len(),
+                    INCOMPLETE_AA_CHAR as char
+                );
                 amino_acids.push(INCOMPLETE_AA_CHAR);
             }
             continue;
         }
-            let nt_triplet: [u8; 3] = codon.try_into().expect("The codon should always be a triplet vector since we've checked for it.");
+        let nt_triplet: [u8; 3] = codon
+            .try_into()
+            .expect("The codon should always be a triplet vector since we've checked for it.");
 
-            let mut num_gaps = 0;
+        let mut num_gaps = 0;
 
-            if !strip_gaps {
-                num_gaps = nt_triplet.iter().filter(|char| **char == GAP_CHAR).count();
-                if (num_gaps == 1) | (num_gaps == 2) {
-                    amino_acids.push(FRAMESHIFT_CHAR);
-                    continue;
-                }
-            }
-
-            let amino_acid = CODON_TABLE
-                .get(&nt_triplet)
-                .with_context(|| format!("Couldn't find amino acid {:?}", String::from_utf8(nt_triplet.to_vec())))
-                .unwrap_or(&&[UNKNOWN_AA_CHAR, ]);
-
-            if ignore_gap_codons & (amino_acid[0].eq(&GAP_CHAR)) {
+        if !strip_gaps {
+            num_gaps = nt_triplet.iter().filter(|char| **char == GAP_CHAR).count();
+            if (num_gaps == 1) | (num_gaps == 2) {
+                amino_acids.push(FRAMESHIFT_CHAR);
                 continue;
-            } else {
-                amino_acids.push(amino_acid.clone()[0]);
             }
+        }
 
+        let amino_acid = CODON_TABLE
+            .get(&nt_triplet)
+            .with_context(|| {
+                format!(
+                    "Couldn't find amino acid {:?}",
+                    String::from_utf8(nt_triplet.to_vec())
+                )
+            })
+            .unwrap_or(&&[UNKNOWN_AA_CHAR]);
+
+        if ignore_gap_codons & (amino_acid[0].eq(&GAP_CHAR)) {
+            continue;
+        } else {
+            amino_acids.push(amino_acid.clone()[0]);
+        }
     }
-
 
     Ok(amino_acids)
 }
 
-pub fn main(){
-
-}
-
+pub fn main() {}
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn basic_test(){
+    fn basic_test() {
         let dna_seq = "ATGTTATAA";
         let expected_translation = "ML*";
         let translation = translate(dna_seq.as_bytes(), false, false).unwrap();
 
         assert_eq!(expected_translation.as_bytes(), translation.as_slice());
-
     }
 
     #[test]
-    fn strip_gaps_true(){
+    fn strip_gaps_true() {
         let dna_seq = "ATGTTA-TAA";
         let expected_translation = "ML*";
         let translation = translate(dna_seq.as_bytes(), true, false).unwrap();
 
         assert_eq!(expected_translation.as_bytes(), translation.as_slice());
-
     }
 
     #[test]
-    fn strip_gaps_false(){
+    fn strip_gaps_false() {
         let dna_seq = "ATGTTA-TAA";
         let expected_translation = "MLX?";
         let translation = translate(dna_seq.as_bytes(), false, false).unwrap();
 
         assert_eq!(expected_translation.as_bytes(), translation.as_slice());
-
     }
 
     #[test]
-    fn ignore_gap_codons(){
+    fn ignore_gap_codons() {
         let dna_seq = "ATGTTA---TAA";
         let expected_translation = "ML*";
         let translation = translate(dna_seq.as_bytes(), false, true).unwrap();
 
         assert_eq!(expected_translation.as_bytes(), translation.as_slice());
-
     }
 
     // TODO: Add more tests lol
