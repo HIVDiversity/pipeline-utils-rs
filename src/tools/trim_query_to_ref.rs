978bc2fd2d7ae921be44e7f08f7f01be945db3a8
Daniel Le Jeune <daniel.h.lejeune@gmail.com>
2025-04-29 11:59:44 +0200

    preliminary new method of getting consensus by checking if seq starts with M

--- src/tools/trim_query_to_ref.rs
+++ src/tools/trim_query_to_ref.rs
@@ -1,15 +1,14 @@
+use crate::utils::translate::translate;
 use anyhow::{Context, Result};
 use bio::alignment::pairwise::*;
 use bio::alignment::Alignment;
 use bio::io::fasta;
+use colored::Colorize;
 use std::collections::HashMap;
 use std::iter::Iterator;
 use std::path::PathBuf;
-use colored::Colorize;
-use crate::utils::translate::translate;
 
-const VERSION: &str = "0.3.5";
-
+const VERSION: &str = "0.3.6";
 
 // TODO: Move readfasta to the utils crate
 fn read_fasta(fasta_file: &PathBuf) -> Result<Vec<Vec<u8>>> {
@@ -39,7 +38,7 @@ pub fn run(
     strip_gaps: bool,
     output_type: &String,
     gap_open_penalty: i32,
-    gap_extend_penalty: i32
+    gap_extend_penalty: i32,
 ) -> Result<()> {
     simple_logger::SimpleLogger::new().env().init()?;
 
@@ -50,7 +49,11 @@ pub fn run(
             .bright_green()
     );
 
-    log::info!("Using a gap open penalty of {} and a gap extend penalty of {}", gap_open_penalty, gap_extend_penalty);
+    log::info!(
+        "Using a gap open penalty of {} and a gap extend penalty of {}",
+        gap_open_penalty,
+        gap_extend_penalty
+    );
 
     let reference_read = read_fasta(reference_file)?;
     let reference = reference_read[0].as_slice();
@@ -67,11 +70,12 @@ pub fn run(
         .yclip(MIN_SCORE)
         .xclip(-10);
 
-    let mut aligner =
-        Aligner::with_capacity_and_scoring(query.len() / 3, ref_aa.len(), scoring);
+    let mut aligner = Aligner::with_capacity_and_scoring(query.len() / 3, ref_aa.len(), scoring);
 
+    let mut best_functional_score = 0;
     let mut best_score = 0;
-    let mut best_frame = 0;
+    let mut best_functional_frame: Option<usize> = None;
+    let mut best_frame: usize = 0;
     let mut best_translation: Vec<u8> = Vec::with_capacity(query.len() / 3);
     let mut best_alignment: Alignment = Default::default();
 
@@ -87,18 +91,52 @@ pub fn run(
             frame,
             alignment.score
         );
-        if alignment.score > best_score {
-            best_score = alignment.score;
-            best_frame = frame;
-            best_translation = cons_aa[alignment.xstart..alignment.xend].to_vec();
-            best_alignment = alignment.clone();
+        log::info!(
+            "\n{}",
+            alignment.pretty(cons_aa.as_slice(), ref_aa_slice, 120)
+        );
+        let putative_translation = cons_aa[alignment.xstart..alignment.xend].to_vec();
+
+        let translation_starts_with_m =
+            putative_translation.get(0).unwrap_or(&b"?"[0]).eq(&b"M"[0]);
+        let alignment_has_better_score = alignment.score > best_functional_score;
+
+        match (translation_starts_with_m, alignment_has_better_score) {
+            (true, true) => {
+                best_functional_score = alignment.score;
+                best_score = alignment.score;
+                best_functional_frame = Some(frame);
+                best_frame = frame;
+                best_translation = putative_translation.clone();
+                best_alignment = alignment.clone();
+            }
+            (true, false) => {}
+            (false, true) => {
+                log::info!("The translation in frame {:?} had a better score {:?} than the previous best {:?} in frame {:?}. We're still going to keep the previous best, but this could indicate a frameshift in the alignment.",
+                frame, alignment.score, best_functional_score,best_functional_frame);
+                best_score = alignment.score;
+                best_frame = frame;
+            }
+            (false, false) => {}
         }
     }
+
+    best_frame = match best_functional_frame{
+        Some(frame) => frame,
+        None =>{
+            log::info!("We were unable to find a frame that started with an M. We'll thus use the sequence with the best alignment score, even though it doesn't start with an M.");
+
+            best_translation = translate(&query[best_frame..], true, true, true)?;
+            best_alignment = aligner.custom(best_translation.as_slice(), ref_aa_slice);
+            best_frame
+        }
+    };
+
 
     log::info!(
         "Choosing translation in frame {:?} with score {:?}:\n{:?}",
         best_frame,
-        best_score,
+        best_functional_score,
         String::from_utf8(best_translation.clone())?
     );
     log::info!(
@@ -107,7 +145,14 @@ pub fn run(
         best_alignment.xend
     );
 
-    log::info!("Alignment:\n{}", best_alignment.pretty(translate(&query[best_frame..], true, true, true)?.as_slice(), ref_aa_slice, 120));
+    log::info!(
+        "Alignment:\n{}",
+        best_alignment.pretty(
+            translate(&query[best_frame..], true, true, true)?.as_slice(),
+            ref_aa_slice,
+            120
+        )
+    );
 
     // TODO: Allow outputting both
     if output_type == "AA" {
